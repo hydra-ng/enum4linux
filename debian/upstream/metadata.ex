f0f01a5d0cab9d878b3ccb2fa997bb643cad62c6
joe <joe@rstrike.my.id>
2025-03-20 18:36:20 +0000

    Import enum4linux packages from BlackTrack source

---
+++ debian/upstream/metadata.ex
@@ -0,0 +1,10 @@
+# Example file for upstream/metadata.
+# See https://wiki.debian.org/UpstreamMetadata for more info/fields.
+# Below an example based on a github project.
+
+# Bug-Database: https://github.com/<user>/enum4linux/issues
+# Bug-Submit: https://github.com/<user>/enum4linux/issues/new
+# Changelog: https://github.com/<user>/enum4linux/blob/master/CHANGES
+# Documentation: https://github.com/<user>/enum4linux/wiki
+# Repository-Browse: https://github.com/<user>/enum4linux
+# Repository: https://github.com/<user>/enum4linux.git
